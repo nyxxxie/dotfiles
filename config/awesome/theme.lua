a8ea28819e744be14c81d81719149ca50f8ad946
Nyxxie <nyxxie@protonmail.ch>
2016-11-06 00:08:59 -0500

    Fixed broken gap offset.

--- config/awesome/theme.lua
+++ config/awesome/theme.lua
@@ -102,7 +102,7 @@ theme.awesome_icon = nil
 -- from /usr/share/icons and /usr/share/icons/hicolor will be used.
 theme.icon_theme = nil
 
-theme.useless_gap_width = 20
+theme.useless_gap_width = 32
 
 return theme
 -- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
