d2ed7c62ce202f10651bad31ef68e7361408e6e6
Nyxxie <nyxxxxie@gmail.com>
2017-03-31 14:50:53 -0500

    Added lots of changes to emacs setup, basically making it usable finally.

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -1,63 +1,68 @@
+;; Add stuff to emacs load path
+(add-to-list 'load-path "~/.emacs.d/lisp")
 
-;; Let emacs save my ass without cluttering my directories in the process
-(setq backup-directory-alist `(("." . "~/.emacs_saves/")))
+;; Set the directory where customize stuff will go
+(unless (file-exists-p "~/.emacs.d/custom.el")
+    (write-region "" nil "~/.emacs.d/custom.el"))
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
 
-;; ----------------------------------------------------------------------------
-;; Packages
-;; ----------------------------------------------------------------------------
+(require 'package)
 
-;; Packages to use
-(setq package-list '(
-    evil
-    evil-leader
-))
-
-;; Set package repos
+;; List the repositories containing them
 (setq package-archives '(
-    ("elpa"      . "http://tromey.com/elpa/")
-    ("gnu"       . "http://elpa.gnu.org/packages/")
+    ("elpa" . "http://tromey.com/elpa/")
+    ("gnu" . "http://elpa.gnu.org/packages/")
     ("marmalade" . "http://marmalade-repo.org/packages/")
-))
+    ("melpa" . "http://melpa.org/packages/")))
 
-;; Activate all packages
+;; Activate packages
 (package-initialize)
 
 ;; Fetch the list of packages available
 (unless package-archive-contents
     (package-refresh-contents))
 
-;; Install the missing packages
-(dolist (package package-list)
-    (unless (package-installed-p package)
-        (package-install package)))
+;; Load all plugins + configure them
+(require 'load-directory)
+(load-directory "~/.emacs.d/plugins.d")
 
-;; ----------------------------------------------------------------------------
-;; Evil
-;; ----------------------------------------------------------------------------
+;; Change backup directory (usually created in the same directory of a file).
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
 
-(require 'evil)
-(evil-mode t)
+;; Disk space is cheap.  Lets save lots of stuff.
+(setq delete-old-versions -1)
+(setq version-control t)
+(setq vc-make-backup-files t)
 
-(require 'evil-leader)
-(global-evil-leader-mode)
-(evil-leader/set-leader "\\")
-(evil-leader/set-key
-"b" 'switch-to-buffer
-"w" 'save-buffer)
+;; Let emacs save my ass without cluttering my directories in the process
+(setq auto-save-file-name-transforms '((".*" "~/.emacs_saves/" t)))
+(setq backup-directory-alist `(("." . "~/.emacs_saves/")))
 
-;; ----------------------------------------------------------------------------
-;; Misc
-;; ----------------------------------------------------------------------------
+;; Save history
+(setq savehist-file "~/.emacs.d/savehist")
+(savehist-mode 1)
+(setq history-length t)
+(setq history-delete-duplicates t)
+(setq savehist-save-minibuffer-history 1)
+(setq savehist-additional-variables
+    '(kill-ring
+      search-ring
+      regexp-search-ring))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (evil-leader evil))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; That menubar thing on top is useless to me, remove it
+(menu-bar-mode -1)
+
+;; Same with the welcome screen that pops up in the gui
+(setq inhibit-startup-screen t)
+
+;; Set some text editor preferences
+ (setq c-basic-offset 4)     ; indents 4 chars
+ (setq tab-width 4)          ; and 4 char wide for TAB
+ (setq indent-tabs-mode nil) ; And force use of spaces
+ (turn-on-font-lock)         ; same as syntax on in Vim
+
+;; TODO:
+;;  * UI should be as terminal-compatible as possible (user experience should be virtually identical if they are in a terminal or not)
+;;      * Plugins should be enabled or disabled gracefully if they are terminal compatible or not
+;;  * Make a separate config dir for each plugin (downloads it, has settings, activates it, etc)
