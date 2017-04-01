ef53aeac39f429e851c5aefa02111b11db029e1b
Nyxxie <nyxxxxie@gmail.com>
2017-04-01 15:53:42 -0500

    Removed some useless stuff.

--- config/emacs/packages/evil.el
+++ config/emacs/packages/evil.el
@@ -1,8 +1,3 @@
-(require 'package)
-
-;; TODO: Ok so I want each of the plugins to be installed and activated with their config values, so replicate this code in a bunch of files with a macro or something so the file takes care of both install and configuration.
 (package-install 'evil)
 (require 'evil)
 (evil-mode 1)
-
-(message "hello from evil")
