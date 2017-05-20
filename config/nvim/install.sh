a9ffe09487091cbc83e01e0e3b107f464ef00201
Nyxxie <nyxxie@protonmail.ch>
2017-05-20 16:04:33 -0500

    Updated install script to install python dependency.

--- config/nvim/install.sh
+++ config/nvim/install.sh
@@ -1,5 +1,10 @@
 mkdir ~/.config/
 ln -s ~/.dotfiles/config/nvim ~/.config/
 
+# Install python depdenceny required by some plugins
+pip2 install --user neovim
+pip3 install --user neovim
+
+# Install plugin manager
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
