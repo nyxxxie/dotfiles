e1ca56b42f6ff696ac60d766220d1aeb3819ccde
Nyxxie <nyxxie@protonmail.ch>
2017-05-20 16:15:19 -0500

    Made plugins install when neovim is first installed.

--- config/nvim/install.sh
+++ config/nvim/install.sh
@@ -8,3 +8,6 @@ pip3 install --user neovim
 # Install plugin manager
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install plugins
+nvim -c ':PlugInstall | q | q'
